4755daf678b02697f6abdbb541d2deb02af831ab
Vladislav134 <137281737+Vladislav134@users.noreply.github.com>
2024-01-16 17:32:23 +0300

    2nd one

---
+++ HW1/1_2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите три числа: ");
+int a = int.Parse(Console.ReadLine()!);
+int b = int.Parse(Console.ReadLine()!);
+int c = int.Parse(Console.ReadLine()!);
+int Maks;
+if (a > b && a > c)
+{ Maks = a}
+if (b > a && b > c)
+{ Maks = b}
+if (c > a && c > b)
+{Maks = c}
+print(Maks);
