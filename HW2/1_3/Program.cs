665b6d3adddcde1068de7206d6938d002514bc71
Vladislav134 <137281737+Vladislav134@users.noreply.github.com>
2024-02-21 02:24:25 +0300

    Yes2

--- HW2/1_3/Program.cs
+++ HW2/1_3/Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-
+void TakeNum(int Num)
+{
+    if (Num < 6 && Num > 0)
+    {
+        Console.WriteLine("Нет");
+    }
+    else if (Num>5 && Num<8)
+    {
+        Console.WriteLine("Да");
+            }
+    else 
+        Console.WriteLine("Введеное число неверно");
+}
+int NumA = int.Parse(Console.ReadLine()!);
+TakeNum(NumA);
