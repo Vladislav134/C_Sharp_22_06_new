f9393a5217d879dca443ab34f992c5d0cc835082
Vladislav134 <137281737+Vladislav134@users.noreply.github.com>
2024-02-28 18:03:45 +0300

    Ura

--- HW4/4_1/Program.cs
+++ HW4/4_1/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+int Degr(int num, int deg)
+{
+int DegNum = 1;
+    for (int i = 1; i <= deg; i++)
+        {
+            DegNum = DegNum * num;
+        }
+return DegNum;
+}
+int a = int.Parse(Console.ReadLine()!);
+int b = int.Parse(Console.ReadLine()!);
+int result = Degr(a, b);
+Console.WriteLine(result);
