a5506a98863ed5d5dec70ea827789dbc82364ff7
Vladislav134 <137281737+Vladislav134@users.noreply.github.com>
2024-03-07 15:02:28 +0300

    Yeah

--- HW4/4_2/Program.cs
+++ HW4/4_2/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// See https://aka.ms/new-console-template for more informat
+int QouNum(int num)
+{
+  int qount = 0;
+  for (int i = 0; i < num;)
+  {
+     int a = num%10;
+     qount = qount + a;
+     num = num/10;
+  }  
+  return qount;
+}
+int b = int.Parse(Console.ReadLine()!);
+int result = QouNum(b);
+Console.WriteLine(result);
