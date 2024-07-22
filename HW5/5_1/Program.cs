87fd4e261063dc11c4fa200b359465b46a99da0c
Vladislav134 <137281737+Vladislav134@users.noreply.github.com>
2024-07-23 00:25:38 +0300

    opa

--- HW5/5_1/Program.cs
+++ HW5/5_1/Program.cs
@@ -1,2 +1,39 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+void Print(int[] arr)
+{
+int size = arr.Length;
+
+for (int i = 0; i < size; i++)
+Console.Write($"{arr[i]} ");
+Console.WriteLine();
+}
+
+int[] MassNums(int size, int from, int to)
+{
+int[] arr = new int[size];
+
+for (int i = 0; i < size; i++)
+arr[i] = new Random().Next(from, to + 1);
+return arr;
+}
+
+void Chetn(int[] arr)
+{
+int summchet;
+summchet = 0;
+
+for (int i = 0; i < arr.Length; i++)
+{
+if (arr[i] % 2 == 0)
+summchet += arr[i];
+}
+Console.WriteLine($"Summa: {summchet}");
+}
+
+int num = int.Parse(Console.ReadLine()!);
+int start = int.Parse(Console.ReadLine()!);
+int stop = int.Parse(Console.ReadLine()!);
+
+int[] mass = MassNums(num, start, stop);
+Print(mass);
+Chetn(mass);
