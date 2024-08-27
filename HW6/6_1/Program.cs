59e8b1d3abc95d437ffbaaa4fa63331692d4b7ce
Vladislav134 <137281737+Vladislav134@users.noreply.github.com>
2024-08-27 17:30:56 +0300

    yup

--- HW6/6_1/Program.cs
+++ HW6/6_1/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int [,] CreateMatrix(int rowCount, int columnCount)
+{
+    int [,] matrix new int [rowCount, columnCount];
+    Random rnd = new Random();
+    for (int =0; i < matrix.Getlength(0); i++)
+    [
+        for (int j = 0; j < matrix.Getlength(1); j++);
+        {
+            matrix[i,j] = rnd.next(1,11);
+        }
+    ]
+    return matrix;
+}
+
+
