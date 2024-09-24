bcb2901c39fa44629970e1dfa1265d8e22019f89
Vladislav134 <137281737+Vladislav134@users.noreply.github.com>
2024-09-24 03:47:00 +0300

    matrix]

--- HW6/6_1/Program.cs
+++ HW6/6_1/Program.cs
@@ -2,7 +2,7 @@
 {
     int [,] matrix new int [rowCount, columnCount];
     Random rnd = new Random();
-    for (int =0; i < matrix.Getlength(0); i++)
+    for (int i = 0; i < matrix.Getlength(0); i++)
     [
         for (int j = 0; j < matrix.Getlength(1); j++);
         {
@@ -12,4 +12,17 @@
     return matrix;
 }
 
+void ShowMatrix (int[,] matrix)
+ 
+for ( int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+
+int [,] matrix = CreateMatrix (4, 5);
+ShowMatrix (matrix);
 
