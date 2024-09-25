26e6aeeb77db504791c230da5845abfde0d04552
Vladislav134 <137281737+Vladislav134@users.noreply.github.com>
2024-09-25 04:08:44 +0300

    ura

--- HW6/6_1/Program.cs
+++ HW6/6_1/Program.cs
@@ -1,20 +1,39 @@
-﻿int [,] CreateMatrix(int rowCount, int columnCount)
+﻿
+int[,] CreateMatrix (int rowCount, int columnCount)    
 {
-    int [,] matrix new int [rowCount, columnCount];
+    int[,] matrix = new int [rowCount, columnCount];
+
+
     Random rnd = new Random();
-    for (int i = 0; i < matrix.Getlength(0); i++)
-    [
-        for (int j = 0; j < matrix.Getlength(1); j++);
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
         {
-            matrix[i,j] = rnd.next(1,11);
+            matrix[i,j] = rnd.Next(1,11);
         }
-    ]
+    }
     return matrix;
 }
 
+int[,] CreateMatrix2 (int rowCount, int columnCount)
+
+{
+    int[,] matrix2 = new int [rowCount, columnCount];
+
+    Random rnd = new Random();
+    for (int x = 0; x < matrix2.GetLength(0); x++)
+    {
+        for (int y = 0; y < matrix2.GetLength(1); y++)
+        {
+            matrix2[x,y] = rnd.Next(1,11);
+        }
+    }
+    return matrix2;
+}    
+
 void ShowMatrix (int[,] matrix)
- 
-for ( int i = 0; i < matrix.GetLength(0); i++)
+{ 
+    for (int i = 0; i < matrix.GetLength(0); i++)
     {
         for (int j = 0; j < matrix.GetLength(1); j++)
         {
@@ -22,7 +41,47 @@ for ( int i = 0; i < matrix.GetLength(0); i++)
         }
         Console.WriteLine();
     }
+}
 
-int [,] matrix = CreateMatrix (4, 5);
-ShowMatrix (matrix);
+void ShowMatrix2 (int[,] matrix2)
+{
+    for ( int x = 0; x < matrix2.GetLength(0); x++)
+    {
+        for (int y = 0; y < matrix2.GetLength(1); y++)
+        {
+            Console.Write($"{matrix2[x,y]} ");
+        }
+        Console.WriteLine();
+    }
+}
+    
+int[,] matrix = CreateMatrix(4,5);
+ShowMatrix(matrix);
+int[,] matrix2 = CreateMatrix2(4,5);
+ShowMatrix2 (matrix2);
 
+for ( int x = 0; x < matrix2.GetLength(0); x++) 
+   {
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int y = 0; y < matrix2.GetLength(1); y++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix2[x,y] = matrix[i,j];
+        }
+    }           
+   }       
+   }
+void ShowMatrix3 (int[,] matrix2) 
+{ 
+    for (int x = 0; x < matrix.GetLength(0); x++)
+    {
+        for (int y = 0; y < matrix.GetLength(1); y++)
+        {
+            Console.Write($"{matrix[x,y]} ");
+        }
+        Console.WriteLine();
+    }
+}     
+ShowMatrix3 (matrix2);
