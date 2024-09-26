3d6157bb5b563b8b34cfa7f503a7399675fb5898
Vladislav134 <137281737+Vladislav134@users.noreply.github.com>
2024-09-27 02:06:29 +0300

    uyp

--- HW7/7_3/Program.cs
+++ HW7/7_3/Program.cs
@@ -1,1 +1,50 @@
-﻿
+﻿int[,] CreateMatrix(int rowCount, int columnCount)
+{
+    int[,] matrix = new int[rowCount, columnCount];
+
+
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(1, 11);
+        }
+    }
+    return matrix;
+}
+
+void ShowMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]},   ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void AveMatrix(int[,] matrix)
+{
+    int AveNum;
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        int Sum = 0;
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            Sum = Sum + matrix[i, j];
+        }
+        AveNum = Sum / matrix.GetLength(1);
+        Console.WriteLine($"AverageCol_{j+1}={AveNum}");
+    }
+}
+
+Console.WriteLine("Введите количество строк");
+int rowCount = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите количество столбцов");
+int columnCount = int.Parse(Console.ReadLine()!);
+int[,] matrix = CreateMatrix(rowCount, columnCount);
+ShowMatrix(matrix);
+AveMatrix(matrix);
