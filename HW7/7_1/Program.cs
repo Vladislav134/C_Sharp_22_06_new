10fb18393e574becdfafff0de6f3cf1c309966f6
Vladislav134 <137281737+Vladislav134@users.noreply.github.com>
2024-09-27 00:33:14 +0300

    yup

--- HW7/7_1/Program.cs
+++ HW7/7_1/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double[,] CreateMatrix(int rowCount, int columnCount)
+{
+    double[,] matrix = new double[rowCount, columnCount];
+
+
+    Random rand = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = Math.Round(rand.Next(-10, 10) * 0.1, 2);
+        }
+    }
+    return matrix;
+}
+
+void ShowMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("Введите количество строк");
+int rowCount = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите количество столбцов");
+int columnCount = int.Parse(Console.ReadLine()!);
+double[,] matrix = CreateMatrix(rowCount, columnCount);
+ShowMatrix(matrix);
