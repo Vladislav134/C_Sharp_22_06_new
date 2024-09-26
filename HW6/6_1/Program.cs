d18ec7066a1a3ea618d0ca7c9dc2aec9ee1bc705
Vladislav134 <137281737+Vladislav134@users.noreply.github.com>
2024-09-26 23:01:27 +0300

    ig

--- HW6/6_1/Program.cs
+++ HW6/6_1/Program.cs
@@ -1,7 +1,7 @@
 ﻿
-int[,] CreateMatrix (int rowCount, int columnCount)    
+int[,] CreateMatrix(int rowCount, int columnCount)
 {
-    int[,] matrix = new int [rowCount, columnCount];
+    int[,] matrix = new int[rowCount, columnCount];
 
 
     Random rnd = new Random();
@@ -9,79 +9,79 @@ int[,] CreateMatrix (int rowCount, int columnCount)
     {
         for (int j = 0; j < matrix.GetLength(1); j++)
         {
-            matrix[i,j] = rnd.Next(1,11);
+            matrix[i, j] = rnd.Next(1, 11);
         }
     }
     return matrix;
 }
 
-int[,] CreateMatrix2 (int rowCount, int columnCount)
+int[,] CreateMatrix2(int rowCount, int columnCount)
 
 {
-    int[,] matrix2 = new int [rowCount, columnCount];
+    int[,] matrix2 = new int[rowCount, columnCount];
 
     Random rnd = new Random();
     for (int x = 0; x < matrix2.GetLength(0); x++)
     {
         for (int y = 0; y < matrix2.GetLength(1); y++)
         {
-            matrix2[x,y] = rnd.Next(1,11);
+            matrix2[x, y] = rnd.Next(1, 11);
         }
     }
     return matrix2;
-}    
+}
 
-void ShowMatrix (int[,] matrix)
-{ 
+void ShowMatrix(int[,] matrix)
+{
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
         for (int j = 0; j < matrix.GetLength(1); j++)
         {
-            Console.Write($"{matrix[i,j]} ");
+            Console.Write($"{matrix[i, j]} ");
         }
         Console.WriteLine();
     }
 }
 
-void ShowMatrix2 (int[,] matrix2)
+void ShowMatrix2(int[,] matrix2)
 {
-    for ( int x = 0; x < matrix2.GetLength(0); x++)
+    for (int x = 0; x < matrix2.GetLength(0); x++)
     {
         for (int y = 0; y < matrix2.GetLength(1); y++)
         {
-            Console.Write($"{matrix2[x,y]} ");
+            Console.Write($"{matrix2[x, y]} ");
         }
         Console.WriteLine();
     }
 }
-    
-int[,] matrix = CreateMatrix(4,5);
-ShowMatrix(matrix);
-int[,] matrix2 = CreateMatrix2(matrix.GetLength(0),matrix.GetLength(1));
-ShowMatrix2 (matrix2);
 
-for ( int x = 0; x < matrix2.GetLength(0); x++) 
-   {
+int[,] matrix = CreateMatrix(4, 5);
+ShowMatrix(matrix);
+int[,] matrix2 = CreateMatrix2(matrix.GetLength(0), matrix.GetLength(1));
+ShowMatrix2(matrix2);
+
+for (int x = 0; x < matrix2.GetLength(0); x++)
+{
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
         for (int y = 0; y < matrix2.GetLength(1); y++)
         {
             for (int j = 0; j < matrix.GetLength(1); j++)
-        {
-            matrix2[x,y] = matrix[i,j];
+            {
+                matrix2[x, y] = matrix[i, j];
+            }
         }
-    }           
-   }       
-   }
-void ShowMatrix3 (int[,] matrix2) 
-{ 
+    }
+}
+void ShowMatrix3(int[,] matrix2)
+{
     for (int x = 0; x < matrix.GetLength(0); x++)
     {
         for (int y = 0; y < matrix.GetLength(1); y++)
         {
-            Console.Write($"{matrix[x,y]} ");
+            Console.Write($"{matrix[x, y]} ");
         }
         Console.WriteLine();
     }
-}     
-ShowMatrix3 (matrix2);
+}
+ShowMatrix3(matrix2);
--- HW6/6_2/Program.cs
+++ HW6/6_2/Program.cs
@@ -1,14 +1,14 @@
 ﻿int PozNum(int num)
 {
-  int pozqount = 0;
-  for (int i = 0; i < num; i++)
-  {
-    Console.WriteLine("Vvedite hislo");
-    int a = int.Parse(Console.ReadLine()!);
-    if (a > 0)
-    pozqount = pozqount + 1;
-  }  
-  return pozqount;
+    int pozqount = 0;
+    for (int i = 0; i < num; i++)
+    {
+        Console.WriteLine("Vvedite hislo");
+        int a = int.Parse(Console.ReadLine()!);
+        if (a > 0)
+            pozqount = pozqount + 1;
+    }
+    return pozqount;
 }
 int result = PozNum(5);
 Console.WriteLine($"Kol. polozhit. chisel: {result}");
